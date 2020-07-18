91876602c0b2792830e1bfa2e532811038a9a592
eNipu <tokhandaker@outlook.com>
2020-07-19 00:44:33 +0900

    initial commit

---
+++ src/main/java/com/alamin/lunchscheduler/LunchschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.alamin.lunchscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alamin/lunchscheduler/LunchschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.lunchscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
